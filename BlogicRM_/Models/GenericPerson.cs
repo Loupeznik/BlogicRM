43e72dbeed38f0ab412cff6f1a07fdfa40dc6820
Dominik Zarsky <43666286+Loupeznik@users.noreply.github.com>
2021-04-24 12:50:13 +0200

    add validation

--- BlogicRM_/Models/GenericPerson.cs
+++ BlogicRM_/Models/GenericPerson.cs
@@ -12,10 +12,12 @@ namespace BlogicRM_.Models
         [Required]
         public string Surname { get; set; }
 
+        [DataType(DataType.EmailAddress)]
         [Display(Name = "Email")]
         [Required]
         public string Email { get; set; }
 
+        [RegularExpression(@"^[0-9]{6}\/[0-9]{3,4}$", ErrorMessage = "Neplatné rodné číslo. Rodné číslo musí být ve formátu XXXXXX/YYY nebo XXXXXX/YYYY")]
         [Display(Name = "Rodné číslo")]
         [Required]
         public string BirthNumber { get; set; }
@@ -24,6 +26,7 @@ namespace BlogicRM_.Models
         [Required]
         public int Age { get; set; }
 
+        [DataType(DataType.PhoneNumber)]
         [Display(Name = "Telefonní číslo")]
         [Required]
         public string Phone { get; set; }
